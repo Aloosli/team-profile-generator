ef3dd776993dadaecfc54f586437d1babc7f268d
aloosli <lizzwizz@tuta.io>
2023-02-24 11:47:17 +0000

    changed inquirer to older version to use requiere

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+"use strict";
+globalThis.regeneratorRuntime = require("regenerator-runtime");
+
+
 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -5,69 +9,72 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./src/page-template.js");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+const employeeQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the employee's ID number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the employee's email address?",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is the employee's role?",
+    choices: ["Engineer", "Intern", "Manager"],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's GitHub username?",
+    when: (answers) => answers.role === "Engineer",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school does the intern attend?",
+    when: (answers) => answers.role === "Intern",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the manager's office number?",
+    when: (answers) => answers.role === "Manager",
+  },
+];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+const employees = [];
 
+inquirer.prompt(employeeQuestions).then((answers) => {
+  const { name, id, email, role, github, school, officeNumber } = answers;
 
-inquirer.prompt([
-    // Prompt for the common employee information
-    {
-      type: 'input',
-      name: 'name',
-      message: "What is the employee's name?",
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is the employee's ID?",
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What is the employee's email address?",
-    },
-    // Prompt for the specific employee information based on the employee type
-    {
-      type: 'input',
-      name: 'github',
-      message: "What is the engineer's GitHub username?",
-      when: (answers) => answers.role === 'Engineer',
-    },
-    {
-      type: 'input',
-      name: 'school',
-      message: "What school does the intern attend?",
-      when: (answers) => answers.role === 'Intern',
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: "What is the manager's office number?",
-      when: (answers) => answers.role === 'Manager',
-    },
-  ]);
-  
+  if (role === "Engineer") {
+    employees.push(new Engineer(name, id, email, github));
+  } else if (role === "Intern") {
+    employees.push(new Intern(name, id, email, school));
+  } else if (role === "Manager") {
+    employees.push(new Manager(name, id, email, officeNumber));
+  }
 
-  const employeeQuestions = [
-    // ...
-  ];
-  
-  const employees = []; // array to hold the employee objects
-  
-  // Prompt for the common employee information
-  inquirer.prompt(employeeQuestions).then(({ name, id, email, role, github, school, officeNumber }) => {
-    if (role === "Engineer") {
-      employees.push(new Engineer(name, id, email, github));
-    } else if (role === "Intern") {
-      employees.push(new Intern(name, id, email, school));
-    } else if (role === "Manager") {
-      employees.push(new Manager(name, id, email, officeNumber));
-    }
+  const renderedHTML = render(employees);
+
+  fs.writeFile(outputPath, renderedHTML, (err) => {
+    if (err) throw err;
+    console.log("Team profile page has been created successfully!");
   });
-  
+});
+
   
