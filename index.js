fa5ddd6400009d0fbb90c00effb5f09e20fcb826
aloosli <lizzwizz@tuta.io>
2023-02-25 11:38:14 +0000

    add validation for all user input exept intern school

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import necessary modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,43 +6,73 @@ const { prompt } = require("inquirer");
 
 const { resolve, join } = require("path");
 const { writeFile, existsSync } = require("fs");
-
+// Import the function that renders the output HTML file
 const render = require("./src/page-template.js");
-
+// Define the output directory and file name
 const OUTPUT_DIR = resolve(__dirname, "output");
 const outputPath = join(OUTPUT_DIR, "team.html");
 
 async function init() {
   let employees = [];
+  // Get the manager's information through inquirer prompt
   const { name, id, email, officeNumber } = await prompt([
     {
       type: "input",
       name: "name",
       message: "What is the team manager's name?",
+      validate: function (input) {
+        if (!input) {
+          return "Name is required.";
+        }
+        return true;
+      },
     },
     {
       type: "input",
       name: "id",
       message: "What is the team manager's ID number?",
+      validate: function (input) {
+        // Check that the input is a positive integer
+        const isValid = /^\d+$/.test(input) && parseInt(input) > 0;
+        if (!isValid) {
+          return "Please enter a positive integer for the ID number.";
+        }
+        return true;
+      },
     },
     {
       type: "input",
       name: "email",
       message: "What is the team manager's email address?",
+      validate: function (input) {
+        // Check that the input is a valid email address
+        const isValid = /\S+@\S+\.\S+/.test(input);
+        if (!isValid) {
+          return "Please enter a valid email address.";
+        }
+        return true;
+      },
     },
     {
       type: "input",
       name: "officeNumber",
       message: "What is the team manager's office number?",
+      validate: function (input) {
+        // Check that the input is a positive integer
+        const isValid = /^\d+$/.test(input) && parseInt(input) > 0;
+        if (!isValid) {
+          return "Please enter a positive integer for the office number.";
+        }
+        return true;
+      },
     },
   ]);
-
+  // Create a Manager object using the manager's information and add it to the employees array
   const manager = new Manager(name, id, email, officeNumber);
-
   employees.push(manager);
-
+  
   let addAnotherEmployee = true;
-
+  // Continue prompting the user to add more employees until they choose not to
   while (addAnotherEmployee) {
     let employeeType = await prompt({
       type: "list",
@@ -56,6 +87,7 @@ async function init() {
 
     switch (employeeType.role) {
       case "Engineer":
+        // Get the engineer's information through inquirer prompt and add a new Engineer object to the employees array
         let {
           name: engName,
           id: engId,
@@ -66,26 +98,56 @@ async function init() {
             type: "input",
             name: "name",
             message: "What is the engineer's name?",
+            validate: function (input) {
+                if (!input) {
+                  return "Name is required.";
+                }
+                return true;
+              },
           },
           {
             type: "input",
             name: "id",
             message: "What is the engineer's ID number?",
+            validate: function (input) {
+                // Check that the input is a positive integer
+                const isValid = /^\d+$/.test(input) && parseInt(input) > 0;
+                if (!isValid) {
+                  return "Please enter a positive integer for the ID number.";
+                }
+                return true;
+              },
           },
           {
             type: "input",
             name: "email",
             message: "What is the engineer's email address?",
+            validate: function (input) {
+                // Check that the input is a valid email address
+                const isValid = /\S+@\S+\.\S+/.test(input);
+                if (!isValid) {
+                  return "Please enter a valid email address.";
+                }
+                return true;
+              },
           },
           {
             type: "input",
             name: "github",
             message: "What is the engineer's GitHub username?",
+            validate: function(input) {
+                const validFormat = /^[a-zA-Z][a-zA-Z0-9-]{0,38}[a-zA-Z0-9]$/;
+                if (!validFormat.test(input)) {
+                  return 'Please enter a valid GitHub username.';
+                }
+                return true;
+              }
           },
         ]);
         employees.push(new Engineer(engName, engId, engEmail, github));
         break;
       case "Intern":
+        // Get the intern's information through inquirer prompt and add a new Intern object to the employees array
         const {
           name: intName,
           id: intId,
@@ -96,16 +158,38 @@ async function init() {
             type: "input",
             name: "name",
             message: "What is the intern's name?",
+            validate: function (input) {
+                if (!input) {
+                  return "Name is required.";
+                }
+                return true;
+              },
           },
           {
             type: "input",
             name: "id",
             message: "What is the intern's ID number?",
+            validate: function (input) {
+                // Check that the input is a positive integer
+                const isValid = /^\d+$/.test(input) && parseInt(input) > 0;
+                if (!isValid) {
+                  return "Please enter a positive integer for the ID number.";
+                }
+                return true;
+              },
           },
           {
             type: "input",
             name: "email",
             message: "What is the intern's email address?",
+            validate: function (input) {
+                // Check that the input is a valid email address
+                const isValid = /\S+@\S+\.\S+/.test(input);
+                if (!isValid) {
+                  return "Please enter a valid email address.";
+                }
+                return true;
+              },
           },
           {
             type: "input",
@@ -121,7 +205,7 @@ async function init() {
         break;
     }
   }
-
+  // 
   const renderedHTML = render(employees);
 
   if (!existsSync(OUTPUT_DIR)) {
